e0e82e7848ccd71efe7480b538eb0dbeb5ad7663
samuel.colin <samuel.colin@adventiel.fr>
2022-06-03 16:48:10 +0200

    First commit

---
+++ src/main/java/fr/akoubayo/proxyvotingapp/ProxyVotingAppApplication.java
@@ -0,0 +1,13 @@
+package fr.akoubayo.proxyvotingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyVotingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyVotingAppApplication.class, args);
+	}
+
+}
